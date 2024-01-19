238b5b46cfa05c845384f2094f94d89db84e2ec8
LSASSE <louis.sasse@protonmail.com>
2024-01-19 10:24:07 +0100

    fix: move all class in folder

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
-#include "callback.h"
-#include "sm.h"
+#include "class/callback.h"
+#include "class/sm.h"
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
