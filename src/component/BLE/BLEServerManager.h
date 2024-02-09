7d76746427a28c23aa6728700e709151a7de19db
LSASSE <louis.sasse@protonmail.com>
2024-02-09 09:55:37 +0100

    fix: update bluetooth name

--- src/component/BLE/BLEServerManager.h
+++ src/component/BLE/BLEServerManager.h
@@ -11,7 +11,7 @@
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-#define DEVICE_NAME         "ESP32_BLE"
+#define DEVICE_NAME         "ESP32_BLE_super_mega_awesome"
 
 class BLEServerManager {
 private:
